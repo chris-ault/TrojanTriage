802387afc73f01dae967dbd4297b09346dc19d21
chris-ault <cda17@students.uwf.edu>
2018-09-16 14:14:45 -0500

    Updated inner scanner to new command with throttle limit

--- VMautomation/innerPython.py
+++ VMautomation/innerPython.py
@@ -3,15 +3,24 @@
 # The after the malware is unzipped a defender scan is run on the resulting folder.
 # The defender results are stored in the shared folder causing a refresh.
 #
-import subprocess
+# Defender scan log C:\Users\user\AppData\Local\Temp\MpCmdRun.log
+#
+import sys
 import os.path
 import time
+import subprocess
 
 inputdir = "\\\\Vboxsvr\\sharedrive2"
 outdir = "\\\\Vboxsvr\\sharedrive2\\"
 defdir = "C:\\Program Files\\Windows Defender"
+# Old command offers no throttle limit
+# OLD scanCmd = "MpCmdRun.exe -scan -ScanType 3 -DisableRemediation -hide -file C:\\Users\\user\\Desktop\\scanme"
+scanCmd = "powershell -command Start-MpScan -ScanPath C:\\Users\\user\\Desktop\\scanme -ScanType CustomScan -ThrottleLimit 80"
+storeResultsCmd = "powershell -command Get-MpThreat >> \\\\Vboxsvr\\sharedrive2\\resultpause.txt"
+
+
 file = "virusshare.rar"
-command = "7z e -p123456 " + file + " -oC:\\Users\\user\\Desktop\\scanme"
+command = "7z e -pinfected " + file + " -oC:\\Users\\user\\Desktop\\scanme"
 
 
 os.chdir(inputdir)
@@ -28,8 +37,12 @@ print out
 if "Everything is Ok" in str(out):
     print "Everything is okay"
     os.chdir(defdir)
-    command = "MpCmdRun.exe -scan -3 C:\\Users\\user\\Desktop\\output > result.txt"
-    p = subprocess.Popen(command, stdout=subprocess.PIPE)
-    print p.communicate()
-# C:\Program Files\Windows Defender> ./MpCmdRun.exe -scan -3 C:\Users\user\Desktop\output
-
+    # Scan :
+    #   MpCmdRun.exe  -scan -ScanType 3 -file C:\\Users\\user\\Desktop\\output
+    # Store Results(append) :
+    #   powershell -command Get-MpThreatDetection >> \\\\Vboxsvr\\sharedrive2\\result.txt
+    scanResults = str(os.system(scanCmd))
+    os.system(storeResultsCmd)
+    # This scan had better return 2 or we have issues and no malware
+    print "scan returned: " + str(scanResults)
+    os.system("move \\\\Vboxsvr\\sharedrive2\\resultpause.txt \\\\Vboxsvr\\sharedrive2\\result.txt")
