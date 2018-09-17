810063c24d3e101e3815de81a5cc72aef0d84708
chris-ault <cda17@students.uwf.edu>
2018-09-17 14:03:32 -0500

    VM is cycling through test files successfully

--- VMautomation/innerPython.py
+++ VMautomation/innerPython.py
@@ -1,48 +1,105 @@
 #
-# This script runs inside the VM automating the malware extraction process
-# The after the malware is unzipped a defender scan is run on the resulting folder.
+# This script runs inside the VM automating the malware extraction process on any zip files
+# Then after the malware is unzipped a defender scan is run on the resulting folder.
 # The defender results are stored in the shared folder causing a refresh.
 #
 # Defender scan log C:\Users\user\AppData\Local\Temp\MpCmdRun.log
 #
+# Scan Option A:
+#   MpCmdRun.exe  -scan -ScanType 3 -file C:\\Users\\user\\Desktop\\output
+# Scan Option B:
+#   powershell -command Start-MpScan -ScanPath C:\\Users\\user\\Desktop\\scanme -ScanType CustomScan -ThrottleLimit 70
+# Store Results(append) :
+#   powershell -command Get-MpThreatDetection >> \\\\Vboxsvr\\sharedrive2\\result.txt
+
 import sys
 import os.path
 import time
 import subprocess
+import glob
 
-inputdir = "\\\\Vboxsvr\\sharedrive2"
+# Input Malware
+inputdir = "\\\\Vboxsvr\\sharedrive2\\input"
+# Output Results.txt
 outdir = "\\\\Vboxsvr\\sharedrive2\\"
+# Windows Defender Prefix location
 defdir = "C:\\Program Files\\Windows Defender"
-# Old command offers no throttle limit
-# OLD scanCmd = "MpCmdRun.exe -scan -ScanType 3 -DisableRemediation -hide -file C:\\Users\\user\\Desktop\\scanme"
-scanCmd = "powershell -command Start-MpScan -ScanPath C:\\Users\\user\\Desktop\\scanme -ScanType CustomScan -ThrottleLimit 80"
+# first command offers no throttle limit however disable remediation is good... This is far too slow, seems to be a 30% limit
+# in combination with realtime protection disabled
+scanCmd = "MpCmdRun.exe -Scan -ScanType 3 -DisableRemediation -File C:\\Users\\user\\Desktop\\scanme"
+# scanCmd = "powershell -command Start-MpScan -ScanPath C:\\Users\\user\\Desktop\\scanme -ScanType CustomScan -ThrottleLimit 70"
+# Save Results to txt file
 storeResultsCmd = "powershell -command Get-MpThreat >> \\\\Vboxsvr\\sharedrive2\\resultpause.txt"
+# This is brokenErase any old results
+# This command is broken Clean MpThreat before getting results
+cleanResultsCmd = "powershell -command Remove-MpThreat"
 
-
-file = "virusshare.rar"
-command = "7z e -pinfected " + file + " -oC:\\Users\\user\\Desktop\\scanme"
-
-
+# First we must verify log is clean
+output = (subprocess.Popen("powershell -command Get-MpThreat",stdout=subprocess.PIPE))
+outResult = output.communicate()[0].rstrip()
+if len(outResult) > 0:
+    print "MpThreat Has results already, Fix it"
+    raw_input()
+    exit()
+print "Logs must be clean"
 os.chdir(inputdir)
-while not os.path.lexists(file):
-    time.sleep(1)
-if os.path.isfile(file):
-    print "I spy a little file"
-    print "unzip"
+for inputfile in glob.glob("*.zip"):
+    # Clean old results before next possible file
+    print str(os.system(cleanResultsCmd))
+    file = inputfile
+    print str(file)
+    # file = "virusshare.rar"
+    # file = "VirusShare_ELF_20140617.zip"
+    command = "7z e -pinfected F:\\input\\" + \
+        file + " -oC:\\Users\\user\\Desktop\\scanme\\"
+    print command
+    #print "File Ready to extract, wait and check Get-MpThreat"
+    #raw_input()
     p = subprocess.Popen(command, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     # The following returns to stdin "A\r\n" to Always replace files while unzipping
-    out, err = p.communicate('A\r\n')
-print out
-if "Everything is Ok" in str(out):
-    print "Everything is okay"
-    os.chdir(defdir)
-    # Scan :
-    #   MpCmdRun.exe  -scan -ScanType 3 -file C:\\Users\\user\\Desktop\\output
-    # Store Results(append) :
-    #   powershell -command Get-MpThreatDetection >> \\\\Vboxsvr\\sharedrive2\\result.txt
-    scanResults = str(os.system(scanCmd))
-    os.system(storeResultsCmd)
-    # This scan had better return 2 or we have issues and no malware
-    print "scan returned: " + str(scanResults)
-    os.system("move \\\\Vboxsvr\\sharedrive2\\resultpause.txt \\\\Vboxsvr\\sharedrive2\\result.txt")
+    out, err = p.communicate()
+    print out
+    #
+    # 7Zip returns "Everything is Ok"
+    #
+    if "Everything is Ok" in str(out):
+        print "Files unzipped good"
+        os.chdir(defdir)
+        print "Attempt start scan now... "
+        print scanCmd
+        # scanCmd Returns 1 for failed scan, 2 for good scan, 0 for empty scan
+        scanResults = str(os.system(scanCmd))
+        time.sleep(5)
+        #
+        # Scan couldn't finish or didn't start due to defender issue
+        #
+        if scanResults == '1':
+            print "scan FAILED, store results"
+            print storeResultsCmd
+            os.system(storeResultsCmd)
+            os.system("move \\\\Vboxsvr\\sharedrive2\\resultpause.txt \\\\Vboxsvr\\sharedrive2\\%s.txt" %
+                      file.split('.zip')[0])
+        #
+        # Scan was good lets Save results.
+        #
+        if scanResults == '2':
+            print "scan Completed, Storing Results"
+            print storeResultsCmd            
+            os.system(storeResultsCmd)
+            print "scan returned: " + str(scanResults)
+            os.system("move \\\\Vboxsvr\\sharedrive2\\resultpause.txt \\\\Vboxsvr\\sharedrive2\\%s.txt" %
+                      file.split('.zip')[0])
+        #
+        # Scan had no results, Make a Log anyways
+        #
+        if scanResults == '0':
+            print "Scan Returned Empty :(, Store results"
+            print storeResultsCmd
+            os.system(storeResultsCmd)
+            os.system("move \\\\Vboxsvr\\sharedrive2\\resultpause.txt \\\\Vboxsvr\\sharedrive2\\%s.txt" %
+                      file.split('.zip')[0])
+    #
+    # Cleanup for next zip regardless of scan return
+    #
+    os.system("del /F /Q C:\\Users\\user\\Desktop\\scanme")
--- VMautomation/outerPython.py
+++ VMautomation/outerPython.py
@@ -6,34 +6,57 @@
 import subprocess
 import os.path
 import time
+import glob
+MalDir = "C:\\Users\\captn2\\Documents\\CyberIntern\\VMs\\MalwareArchives\\Test\\"
+OutDir = "C:\\Users\\captn2\\Documents\\CyberIntern\\VMs\\SHAREDRIVE\\input"
 
-directory = "C:\\Users\\captn2\\Documents\\CyberIntern\\VMs\\SHAREDRIVE\\"
-file_path = "result.txt"
 
-result = (subprocess.Popen("""C:\Program Files\Oracle\VirtualBox\VBoxManage.exe snapshot win10 restorecurrent""",
-                           stdout=subprocess.PIPE).communicate()[0])
-if "Restoring" in result:
-    print "good restore.. > booting"
-    result = (subprocess.Popen("""C:\Program Files\Oracle\VirtualBox\VBoxManage.exe startvm win10""",
-                               stdout=subprocess.PIPE).communicate()[0])
-    if "started." in result:
-        print "booted"
-        os.chdir(directory)
-        while not os.path.lexists(file_path):
-            time.sleep(3)
-        if os.path.isfile(file_path):
-            print "I spy a little file"
-            print "shutdown"
-            result = (subprocess.Popen("""C:\Program Files\Oracle\VirtualBox\VBoxManage.exe controlvm win10 poweroff hard""",
-                                       stdout=subprocess.PIPE).communicate())
-            print result
-            if not result[0]:
-                print "Clean Kill"
+#  For loop Move a single file from directory into Share/input Directory
+os.chdir(MalDir)
+for singleZip in glob.glob("*.zip"):
+    print "Working on " + str(singleZip)
+    os.system("move %s%s %s" % (MalDir, singleZip, OutDir))
+    #
+    # Restore most recent snapshot
+    #
+    result = (subprocess.Popen("""C:\Program Files\Oracle\VirtualBox\VBoxManage.exe snapshot win10 restorecurrent""", stdout=subprocess.PIPE).communicate()[0])
+    if "Restoring" in result:
+        print "good restore.. > booting"
+        #
+        # Boot VM
+        #
+        result = (subprocess.Popen("""C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe startvm win10""", stdout=subprocess.PIPE).communicate()[0])
+        if "started." in result:
+            print "booted, Waiting for result file"
+            directory = "C:\\Users\\captn2\\Documents\\CyberIntern\\VMs\\SHAREDRIVE\\"
+            results = singleZip.split('.zip')[0] + str('.txt')
+            os.chdir(directory)
+            while not os.path.lexists(results):
+                time.sleep(3)
+            if os.path.isfile(results):
+                print "I spy a little file, delay 10 seconds for it to finish copying"
+                time.sleep(10)
+                print "shutdown"
+                #
+                # Shut down VM File found
+                #
+                result = (subprocess.Popen("""C:\Program Files\Oracle\VirtualBox\VBoxManage.exe controlvm win10 poweroff hard""",
+                                           stdout=subprocess.PIPE).communicate())
+                print result
+                if not result[0]:
+                    print "Clean Kill"
+                else:
+                    print "Couldn't shut down"
             else:
-                print "Couldn't shut down"
+                raise ValueError("%s not a file" % results)
         else:
-            raise ValueError("%s not a file" % file_path)
+            print "couldn't start"
     else:
-        print "couldn't start"
-else:
-    print "No Restore"
+        print "No Restore"
+    # Move single file back where it came from
+    print ("Removing old file back to Store")
+    os.chdir(OutDir)
+    os.system("move %s %s" % ( singleZip, MalDir))
+    print "Let the VM close up"
+    time.sleep(15)
+print "Done"
